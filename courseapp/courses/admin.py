c8dffb8a2c4043db34acbcad583eb88e544e2013
Hoang Nguyen Quoc Duy <quocduy6114@gmail.com>
2023-11-12 14:57:44 +0700

    create dao

--- courseapp/courses/admin.py
+++ courseapp/courses/admin.py
@@ -1,9 +1,35 @@
 from django import forms
 from django.contrib import admin
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django.db.models import Count
+from django.template.response import TemplateResponse
+from django.urls import path
 from django.utils.safestring import mark_safe
 
 from .models import Category, Course, Lesson, Tag
+from courses import dao
+
+class CourseAppAdminSite(admin.AdminSite):
+    site_header = 'COURSES MANAGE SYSTEM'
+
+    def get_urls(self):
+        return [
+                   path('course-stats/', self.stats_view)
+               ] + super().get_urls()
+
+    def stats_view(self, request):
+        count = Course.objects.filter(active=True).count()
+
+        stats = Course.objects \
+        .annotate(lesson_count=Count('category__id')) \
+        .values('id', 'subject', 'lesson_count')
+        return TemplateResponse(request,
+                            'admin/stats.html', {
+                                'course_count': dao.count_course_by_cate()
+                            })
+
+
+admin_site = CourseAppAdminSite(name='myadmin')
 
 
 class CourseForm(forms.ModelForm):
@@ -32,7 +58,7 @@ class CourseAdmin(admin.ModelAdmin):
             )
 
 
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Course, CourseAdmin)
-admin.site.register(Lesson)
-admin.site.register(Tag)
+admin_site.register(Category, CategoryAdmin)
+admin_site.register(Course, CourseAdmin)
+admin_site.register(Lesson)
+admin_site.register(Tag)
--- courseapp/courses/models.py
+++ courseapp/courses/models.py
@@ -28,7 +28,7 @@ class Course(BaseModel):
     subject = models.CharField(max_length=255, null=False)
     description = RichTextField()
     image = models.ImageField(upload_to='courses/%Y/%m')
-    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT, related_query_name='course')
 
     def __str__(self):
         return self.subject
